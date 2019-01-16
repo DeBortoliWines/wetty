c00ae353bac64f3413f54b9b09f1c3a1a12a1447
Ben Letchford <ben_letchford@debortoli.com.au>
2019-01-17 10:06:56 +1100

    File transfer using control characters via blob

--- src/client/index.ts
+++ src/client/index.ts
@@ -12,8 +12,12 @@ const socket = io(window.location.origin, {
   path: `${trim(socketBase)}/socket.io`,
 });
 
+const FILE_BEGIN = '^[[5i\r\n';
+const FILE_END = '\r\n^[[4i\r\n';
+
 socket.on('connect', () => {
   const term = new Terminal();
+  let pdfBuffer = [];
   term.open(document.getElementById('terminal'));
   term.setOption('fontSize', 14);
   document.getElementById('overlay').style.display = 'none';
@@ -44,6 +48,34 @@ socket.on('connect', () => {
     disconnect(data);
   }
 
+  function onCompleteFile() {
+    let bufferCharacters = pdfBuffer.join('');
+    bufferCharacters = bufferCharacters.substring(
+      bufferCharacters.lastIndexOf(FILE_BEGIN) + FILE_BEGIN.length,
+      bufferCharacters.lastIndexOf(FILE_END)
+    );
+
+    // Try to decode it as base64, if it fails we assume it's not base64
+    try {
+      bufferCharacters = window.atob(bufferCharacters);
+    } catch (err) {}
+
+    const bytes = new Uint8Array(bufferCharacters.length);
+    for (let i = 0; i < bufferCharacters.length; i++) {
+      bytes[i] = bufferCharacters.charCodeAt(i);
+    }
+
+    let mimeType = 'application/octet-stream';
+    if (bufferCharacters.indexOf('%PDF') === 0) mimeType = 'application/pdf';
+
+    const blob = new Blob([new Uint8Array(bytes.buffer)], { type: mimeType });
+    const blobUrl = URL.createObjectURL(blob);
+
+    window.open(blobUrl);
+
+    pdfBuffer = [];
+  }
+
   term.on('data', data => {
     socket.emit('input', data);
   });
@@ -52,7 +84,31 @@ socket.on('connect', () => {
   });
   socket
     .on('data', (data: string) => {
-      term.write(data);
+      const indexOfFileBegin = data.indexOf(FILE_BEGIN);
+      const indexOfFileEnd = data.indexOf(FILE_END);
+
+      // If we've got the entire file in one chunk
+      if (indexOfFileBegin !== -1 && indexOfFileEnd !== -1) {
+        pdfBuffer.push(data);
+        onCompleteFile();
+      }
+      // If we've found a beginning marker
+      else if (indexOfFileBegin !== -1) {
+        pdfBuffer.push(data);
+      }
+      // If we've found an ending marker
+      else if (indexOfFileEnd !== -1) {
+        pdfBuffer.push(data);
+        onCompleteFile();
+      }
+      // If we've found the continuation of a file
+      else if (pdfBuffer.length > 0) {
+        pdfBuffer.push(data);
+      }
+      // Just treat it as normal data
+      else {
+        term.write(data);
+      }
     })
     .on('login', () => {
       term.writeln('');
