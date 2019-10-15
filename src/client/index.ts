768ab206e42b867b6f19b13a15b86ea39e7c1213
Ben Letchford <contact@benl.com.au>
2019-10-15 16:59:39 +1100

    Show file links via a popup

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,6 +3,7 @@ import { isUndefined } from 'lodash';
 import * as io from 'socket.io-client';
 import * as fileType from 'file-type';
 import { fit } from 'xterm/lib/addons/fit/fit';
+import Toastify from 'toastify-js';
 import './wetty.scss';
 import './favicon.ico';
 
@@ -59,23 +60,43 @@ socket.on('connect', () => {
     // Try to decode it as base64, if it fails we assume it's not base64
     try {
       bufferCharacters = window.atob(bufferCharacters);
-    } catch (err) {}
+    } catch (err) {
+      // Assuming it's not base64...
+    }
 
     const bytes = new Uint8Array(bufferCharacters.length);
-    for (let i = 0; i < bufferCharacters.length; i++) {
+    for (let i = 0; i < bufferCharacters.length; i += 1) {
       bytes[i] = bufferCharacters.charCodeAt(i);
     }
 
     let mimeType = 'application/octet-stream';
-    let typeData = fileType(bytes);
-    if (typeData) mimeType = typeData.mime;
+    let fileExt = '';
+    const typeData = fileType(bytes);
+    if (typeData) {
+      mimeType = typeData.mime;
+      fileExt = typeData.ext;
+    }
+    const fileName = `file-${new Date()
+      .toISOString()
+      .split('.')[0]
+      .replace(/-/g, '')
+      .replace('T', '')
+      .replace(/:/g, '')}${fileExt ? `.${fileExt}` : ''}`;
 
     const blob = new Blob([new Uint8Array(bytes.buffer)], { type: mimeType });
     const blobUrl = URL.createObjectURL(blob);
 
-    window.open(blobUrl);
-
     fileBuffer = [];
+
+    Toastify({
+      text: `Download ready: <a href="${blobUrl}" target="_blank" download="${fileName}">${fileName}</a>`,
+      duration: 10000,
+      newWindow: true,
+      gravity: 'bottom',
+      position: 'right',
+      backgroundColor: '#fff',
+      stopOnFocus: true,
+    }).showToast();
   }
 
   term.on('data', data => {
