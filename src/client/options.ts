27b80c65550a1fe40d95a875e77b40db2b9f774c
Ben Letchford <contact@benl.com.au>
2019-10-28 09:39:58 +1100

    dbw styles

--- src/client/options.ts
+++ src/client/options.ts
@@ -1,7 +1,11 @@
 import { isUndefined } from 'lodash';
 
 export default function loadOptions(): object {
-  const defaultOptions = { fontSize: 14 };
+  const defaultOptions = {
+    fontSize: 16,
+    fontFamily: '"DejaVu Sans Mono", "Everson Mono", FreeMono, "Andale Mono", Consolas, monospace',
+    'cursorBlink': true
+  };
   try {
     return isUndefined(localStorage.options)
       ? defaultOptions
