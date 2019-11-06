8ac127c050b98f8029df02bf9b4b78f0b8ce74f2
Ben Letchford <contact@benl.com.au>
2019-11-06 13:16:49 +1100

    wip

--- src/client/download.ts
+++ src/client/download.ts
@@ -7,13 +7,9 @@ const fileBuffer: string[] = [];
 
 export class FileDownloader {
   fileBuffer: string[];
-
   file_begin: string;
-
   file_end: string;
-
   partial_file_begin: string;
-
   partial_file_end: string;
 
   constructor(
@@ -41,76 +37,70 @@ export class FileDownloader {
           this.partial_file_begin = '';
           // return all the data as if it wasn't split across different buffer calls
           return this.buffer(this.file_begin + newData);
-        } 
-          // We found the next expected character but we still haven't got the completed file_begin sequence
-          return newData ? this.buffer(newData) : '';
-        
+        }
+        // We found the next expected character but we still haven't got the completed file_begin sequence
+        return newData ? this.buffer(newData) : '';
       }
       // The next expected character wasn't part of the marker, it was a false positive
       // return all the data as if it wasn't split across different buffer calls
-      
-        const newData = this.partial_file_begin + data;
-        this.partial_file_begin = '';
-        return this.buffer(newData);
-      
-    } 
-      const indexOfFileBegin = data.indexOf(this.file_begin);
-      const indexOfFileEnd = data.indexOf(this.file_end);
 
-      // If we've got the entire file in one chunk
-      if (indexOfFileBegin !== -1 && indexOfFileEnd !== -1) {
-        const bufferCharacters = data.substring(
-          indexOfFileBegin + this.file_begin.length,
-          indexOfFileEnd
-        );
-        remainingCharacters = data.replace(
-          this.file_begin + bufferCharacters + this.file_end,
-          ''
-        );
-        this.fileBuffer.push(bufferCharacters);
-        this.onCompleteFile();
-      }
+      const newData = this.partial_file_begin + data;
+      this.partial_file_begin = '';
+      return this.buffer(newData);
+    }
+    const indexOfFileBegin = data.indexOf(this.file_begin);
+    const indexOfFileEnd = data.indexOf(this.file_end);
 
-      // If we've found a beginning marker
-      else if (indexOfFileBegin !== -1) {
-        const bufferCharacters = data.substring(
-          indexOfFileBegin + this.file_begin.length
-        );
-        remainingCharacters = data.replace(
-          this.file_begin + bufferCharacters,
-          ''
-        );
-        this.fileBuffer.push(bufferCharacters);
-      }
+    // If we've got the entire file in one chunk
+    if (indexOfFileBegin !== -1 && indexOfFileEnd !== -1) {
+      const bufferCharacters = data.substring(
+        indexOfFileBegin + this.file_begin.length,
+        indexOfFileEnd
+      );
+      remainingCharacters = data.replace(
+        this.file_begin + bufferCharacters + this.file_end,
+        ''
+      );
+      this.fileBuffer.push(bufferCharacters);
+      this.onCompleteFile();
+    }
 
-      // If we've found an ending marker
-      else if (indexOfFileEnd !== -1) {
-        const bufferCharacters = data.substring(0, indexOfFileEnd);
-        remainingCharacters = data.replace(
-          bufferCharacters + this.file_end,
-          ''
-        );
-        this.fileBuffer.push(data);
-        this.onCompleteFile();
-      }
+    // If we've found a beginning marker
+    else if (indexOfFileBegin !== -1) {
+      const bufferCharacters = data.substring(
+        indexOfFileBegin + this.file_begin.length
+      );
+      remainingCharacters = data.replace(
+        this.file_begin + bufferCharacters,
+        ''
+      );
+      this.fileBuffer.push(bufferCharacters);
+    }
 
-      // If we're in the middle of buffering a file...
-      else if (this.fileBuffer.length > 0) {
-        this.fileBuffer.push(data);
-      }
+    // If we've found an ending marker
+    else if (indexOfFileEnd !== -1) {
+      const bufferCharacters = data.substring(0, indexOfFileEnd);
+      remainingCharacters = data.replace(bufferCharacters + this.file_end, '');
+      this.fileBuffer.push(data);
+      this.onCompleteFile();
+    }
 
-      // Check if there's potential for the start of a partial file marker
-      // we only check the end of the data
-      else {
-        for (let i = this.file_begin.length - 1; i > 0; i--) {
-          if (this.file_begin.substr(0, i) === data.substr(-i)) {
-            this.partial_file_begin = data.substr(-i);
-            remainingCharacters = data.substr(0, data.length - i);
-            break;
-          }
+    // If we're in the middle of buffering a file...
+    else if (this.fileBuffer.length > 0) {
+      this.fileBuffer.push(data);
+    }
+
+    // Check if there's potential for the start of a partial file marker
+    // we only check the end of the data
+    else {
+      for (let i = this.file_begin.length - 1; i > 0; i--) {
+        if (this.file_begin.substr(0, i) === data.substr(-i)) {
+          this.partial_file_begin = data.substr(-i);
+          remainingCharacters = data.substr(0, data.length - i);
+          break;
         }
       }
-    
+    }
 
     // This is where it gets tricky...
     return remainingCharacters;
--- src/client/specs/download.spec.ts
+++ src/client/specs/download.spec.ts
@@ -26,65 +26,65 @@ describe('FileDownloader', () => {
   });
 
   it('should return data before file markers', () => {
-    const mockFile = `${FILE_BEGIN  }test string${  FILE_END}`;
+    const mockFile = `${FILE_BEGIN}test string${FILE_END}`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
-    expect(fileDownloader.buffer(`DATA AT THE LEFT${  mockFile}`)).to.equal(
+    expect(fileDownloader.buffer(`DATA AT THE LEFT${mockFile}`)).to.equal(
       'DATA AT THE LEFT'
     );
     expect(onCompleteFileStub.calledOnce).to.be.true;
   });
 
   it('should return data after file markers', () => {
-    const mockFile = `${FILE_BEGIN  }test string${  FILE_END}`;
+    const mockFile = `${FILE_BEGIN}test string${FILE_END}`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
-    expect(fileDownloader.buffer(`${mockFile  }DATA AT THE RIGHT`)).to.equal(
+    expect(fileDownloader.buffer(`${mockFile}DATA AT THE RIGHT`)).to.equal(
       'DATA AT THE RIGHT'
     );
     expect(onCompleteFileStub.calledOnce).to.be.true;
   });
 
   it('should return data before and after file markers', () => {
-    const mockFile = `${FILE_BEGIN  }test string${  FILE_END}`;
+    const mockFile = `${FILE_BEGIN}test string${FILE_END}`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
     expect(
-      fileDownloader.buffer(`DATA AT THE LEFT${  mockFile  }DATA AT THE RIGHT`)
+      fileDownloader.buffer(`DATA AT THE LEFT${mockFile}DATA AT THE RIGHT`)
     ).to.equal('DATA AT THE LEFTDATA AT THE RIGHT');
     expect(onCompleteFileStub.calledOnce).to.be.true;
   });
 
   it('should return data before a beginning marker found', () => {
-    const mockFile = `${FILE_BEGIN  }test string`;
+    const mockFile = `${FILE_BEGIN}test string`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
-    expect(fileDownloader.buffer(`DATA AT THE LEFT${  mockFile}`)).to.equal(
+    expect(fileDownloader.buffer(`DATA AT THE LEFT${mockFile}`)).to.equal(
       'DATA AT THE LEFT'
     );
   });
 
   it('should return data after an ending marker found', () => {
-    const mockFilePart1 = `${FILE_BEGIN  }test`;
-    const mockFilePart2 = `string${  FILE_END}`;
+    const mockFilePart1 = `${FILE_BEGIN}test`;
+    const mockFilePart2 = `string${FILE_END}`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
     expect(fileDownloader.buffer(mockFilePart1)).to.equal('');
-    expect(fileDownloader.buffer(`${mockFilePart2  }DATA AT THE RIGHT`)).to.equal(
+    expect(fileDownloader.buffer(`${mockFilePart2}DATA AT THE RIGHT`)).to.equal(
       'DATA AT THE RIGHT'
     );
   });
 
-  it('should buffer across incomplete file markers on two calls', () => {
-    const mockFilePart1 = `DATA AT THE LEFT${  FILE_BEGIN.substr(0, 3)}`;
-    const mockFilePart2 = `${FILE_BEGIN.substr(3)  }FILE DATA`;
+  it('should buffer across incomplete file begin marker sequence on two calls', () => {
+    const mockFilePart1 = `DATA AT THE LEFT${FILE_BEGIN.substr(0, 3)}`;
+    const mockFilePart2 = `${FILE_BEGIN.substr(3)}FILE DATA`;
 
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
     expect(fileDownloader.buffer(mockFilePart1)).to.equal('DATA AT THE LEFT');
     expect(fileDownloader.buffer(mockFilePart2)).to.equal('');
   });
 
-  it('should buffer across incomplete file markers on n calls', () => {
+  it('should buffer across incomplete file begin marker sequence on n calls', () => {
     fileDownloader = new FileDownloader('BEGIN', 'END');
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
 
@@ -96,7 +96,7 @@ describe('FileDownloader', () => {
     expect(onCompleteFileStub.calledOnce).to.be.true;
   });
 
-  it('should buffer across incomplete file markers with data on the left and right on n calls', () => {
+  it('should buffer across incomplete file begin marker sequence with data on the left and right on n calls', () => {
     fileDownloader = new FileDownloader('BEGIN', 'END');
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
 
@@ -112,7 +112,7 @@ describe('FileDownloader', () => {
     expect(onCompleteFileStub.calledOnce).to.be.true;
   });
 
-  it('should buffer across incomplete file markers then handle false positive', () => {
+  it('should buffer across incomplete file begin marker sequence then handle false positive', () => {
     fileDownloader = new FileDownloader('BEGIN', 'END');
     const onCompleteFileStub = sinon.stub(fileDownloader, 'onCompleteFile');
 
